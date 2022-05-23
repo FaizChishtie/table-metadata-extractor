80a526700faffdea05d53f560b4c8b4d27cb3f61
Faizaan Chishtie <faizchishtie@gmail.com>
2022-05-23 15:50:35 -0400

    feat: get end to end table metadata

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from sqlalchemy import MetaData, create_engine, inspect
+from sqlalchemy import MetaData, create_engine, inspect, func, text
+from sqlalchemy.orm import sessionmaker
 from src.models.TableMetadata import TableMetadata
 import json
 
@@ -15,12 +16,12 @@ def retrieve_table_metadata():
 
     metadata = create_metadata_object(connection)
 
-    inspector = inspect(connection)
+    session = sessionmaker(bind=connection)
+    session = session()
 
-    # for table in inspector.get_table_names():
-    #     print(inspector.get_columns(table))
+    tables = get_table_metadata(metadata, uri, session)
 
-    tables = get_table_metadata(metadata)
+    session.close()
 
     return jsonify(tables)
 
@@ -29,14 +30,22 @@ def create_metadata_object(connection):
     metadata.reflect()
     return metadata
 
-def get_table_metadata(metadata):
+def get_table_metadata(metadata, uri, session):
     tables = []
     for key in metadata.tables.keys():
-        tablemetadata_obj = TableMetadata.create_table_from_metadata(metadata.tables[key])
+        num_rows = get_number_of_rows(metadata.tables[key], session)
+        # last_modified = get_last_modified(metadata.tables[key].name, engine)
+        tablemetadata_obj = TableMetadata.create_table_from_metadata(metadata.tables[key], uri, num_rows)
         tables.append(tablemetadata_obj.to_dict())
     
     return tables
 
+def get_number_of_rows(table, session):
+    return session.query(func.count(table.c.id)).scalar()
+
+def get_last_modified(table, engine):
+    sql = text(f'SELECT {table}, [modify_date] FROM sys.tables')
+    return engine.execute(sql)
 
 # start server
 if __name__ == '__main__':
--- src/models/TableMetadata.py
+++ src/models/TableMetadata.py
@@ -15,12 +15,12 @@ class TableMetadata():
         self.name = name
     
     @staticmethod
-    def create_table_from_metadata(table_metadata):
+    def create_table_from_metadata(table_metadata, database_uri, num_rows, last_updated = "last_updated"):
         name = table_metadata.name
         schema = table_metadata.schema
-        num_rows = 0
-        database = "database"
-        last_updated = "lu"
+        num_rows = num_rows
+        database = str(database_uri)
+        last_updated = last_updated # not tracked by psql
         columns = []
 
         for c in table_metadata.c:
@@ -37,7 +37,14 @@ class TableMetadata():
         return f'<TableMetadata {self.name}, {self.schema}, {self.num_rows}, {self.database}, {self.last_updated}{col_string}>'
     
     def to_dict(self):
-        to_return = {"name": self.name, "schema": self.schema, "num_rows": str(self.num_rows), "database": self.database, "last_updated": self.last_updated}
+        to_return = {
+            "name": self.name, 
+            "schema": self.schema, 
+            "num_rows": str(self.num_rows), 
+            "database": self.database, 
+            "last_updated": 
+            self.last_updated
+        }
         cols = []
         for col in self.columns:
             cols.append(col.to_dict())
