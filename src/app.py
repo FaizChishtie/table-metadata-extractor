fe1c815d7ebba3708f1236a54c30ef595c4cf078
Faizaan Chishtie <faizchishtie@gmail.com>
2022-05-23 15:32:22 -0400

    feat: end to end translation nearly done

--- src/app.py
+++ src/app.py
@@ -1,27 +1,42 @@
 from flask import Flask, request, jsonify
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import MetaData, create_engine, inspect
+from src.models.TableMetadata import TableMetadata
 import json
-
-import os
 
 # initialize app
 app = Flask(__name__)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 @app.route('/', methods=['POST'])
-def findTableMetadata():
+def retrieve_table_metadata():
     uri = request.json['uri']
 
+    # todo validate URI
     connection = create_engine(uri)
 
+    metadata = create_metadata_object(connection)
+
+    inspector = inspect(connection)
+
+    # for table in inspector.get_table_names():
+    #     print(inspector.get_columns(table))
+
+    tables = get_table_metadata(metadata)
+
+    return jsonify(tables)
+
+def create_metadata_object(connection):
     metadata = MetaData(bind=connection)
-
     metadata.reflect()
+    return metadata
 
-    return jsonify({
-        'metadata': list(metadata.tables.keys())
-    })
+def get_table_metadata(metadata):
+    tables = []
+    for key in metadata.tables.keys():
+        tablemetadata_obj = TableMetadata.create_table_from_metadata(metadata.tables[key])
+        tables.append(tablemetadata_obj.to_dict())
+    
+    return tables
+
 
 # start server
 if __name__ == '__main__':
--- src/models/TableMetadata.py
+++ src/models/TableMetadata.py
@@ -1,14 +1,46 @@
-from ColumnMetadata import ColumnMetadata
+import json
+from src.models.ColumnMetadata import ColumnMetadata
 
 Columns = list[ColumnMetadata]
 
 class TableMetadata():
     # todo make serializable
 
-    def __init__(self, columns: Columns, num_rows: int, last_updated: str, schema: str, database: str):
+    def __init__(self, columns: Columns, num_rows: int, last_updated: str, schema: str, database: str, name: str):
         self.columns = columns
         self.num_rows = num_rows
         self.last_updated = last_updated
         self.schema = schema
         self.database = database
+        self.name = name
     
+    @staticmethod
+    def create_table_from_metadata(table_metadata):
+        name = table_metadata.name
+        schema = table_metadata.schema
+        num_rows = 0
+        database = "database"
+        last_updated = "lu"
+        columns = []
+
+        for c in table_metadata.c:
+            column_metadata = ColumnMetadata.create_column_from_metadata(c)
+            columns.append(column_metadata)
+
+        return TableMetadata(columns, num_rows, last_updated, schema, database, name)
+
+    def __repr__(self):
+        col_string = ''
+        for c in self.columns:
+            col_string += ', ' + str(c)
+
+        return f'<TableMetadata {self.name}, {self.schema}, {self.num_rows}, {self.database}, {self.last_updated}{col_string}>'
+    
+    def to_dict(self):
+        to_return = {"name": self.name, "schema": self.schema, "num_rows": str(self.num_rows), "database": self.database, "last_updated": self.last_updated}
+        cols = []
+        for col in self.columns:
+            cols.append(col.to_dict())
+
+        to_return["columns"] = cols
+        return to_return
